cd4b429ec8144d2af2beeabbcd89f00a66481504
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2020-07-28 21:07:19 +0200

    task 1 finished

--- task1.ts
+++ task1.ts
@@ -1,1 +1,18 @@
-console.log('Hello world')
+'use strict';
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', () => {
+  let chunk;
+
+  while ((chunk = process.stdin.read()) !== null) {
+    chunk = reverseString(chunk);
+    process.stdout.write(`${chunk}\n\n`);
+  }
+});
+
+function reverseString(inputString: string): string {
+  const splitString: string[] = inputString.split('');
+  splitString.pop();
+  return splitString.reverse().join('');
+}
