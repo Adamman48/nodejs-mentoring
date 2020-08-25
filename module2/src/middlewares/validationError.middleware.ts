d9671a222d43e8440f0fd0d5a10b9f51edea451f
Adam Pisky-Nagy <adam_nagy89@yahoo.com>
2020-08-25 11:29:08 +0200

    Removed unused function declaration

--- module2/src/middlewares/validationError.middleware.ts
+++ module2/src/middlewares/validationError.middleware.ts
@@ -2,10 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import { ExpressJoiError } from "express-joi-validation";
 import { ConsoleColorsEnum } from "../../../utils/consoleUtils";
 
-enum ValidationErrorsEnum {
-
-};
-
 function validationErrorMiddleware(err: ExpressJoiError, req: Request, res: Response, next: NextFunction) {
   if (err && err.error && err.error.isJoi) {
     console.error(ConsoleColorsEnum.RED, err);
